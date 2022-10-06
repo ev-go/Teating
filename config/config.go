d26ccb6a65b63bde6991369ef90a462c74614b01
Dmitry T <dmitry_dtv@mail.ru>
2022-10-06 14:05:04 +0500

    init

--- config/config.go
+++ config/config.go
@@ -1,58 +1,68 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/ilyakaznacheev/cleanenv"
+	//"gitlab.boquar.tech/galileosky/pkg/acl"
 )
 
 type (
-	// Config -.
 	Config struct {
-		App  `yaml:"app"`
-		HTTP `yaml:"http"`
-		Log  `yaml:"logger"`
-		PG   `yaml:"postgres"`
-		RMQ  `yaml:"rabbitmq"`
+		App   `yaml:"app"`
+		Log   `yaml:"log"`
+		PG    `yaml:"pg"`
+		Trace `yaml:"trace"`
+		HTTP  `yaml:"http"`
+		//Acl      *acl.ConfigAcl `yaml:"acl"`
+		GRPC     `yaml:"grpc"`
+		KeyCloak `yaml:"keycloak"`
 	}
 
-	// App -.
 	App struct {
-		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
-		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+		Name        string `yaml:"name" env:"APP_NAME"`
+		Environment string `yaml:"environment" env:"APP_ENVIRONMENT" env-default:"develop"`
 	}
 
-	// HTTP -.
-	HTTP struct {
-		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
-	}
-
-	// Log -.
 	Log struct {
-		Level string `env-required:"true" yaml:"log_level"   env:"LOG_LEVEL"`
+		//Level string `yaml:"level" env:"LOG_LEVEL" env-default:"info"`
+		Level string `env-required:"true" yaml:"level"   env:"LOG_LEVEL"`
 	}
 
-	// PG -.
 	PG struct {
-		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
-		URL     string `env-required:"true"                 env:"PG_URL"`
+		RSID        string `yaml:"rsid" env:"RSID"`
+		PoolMax     int    `yaml:"pool_max" env:"PG_POOL_MAX"`
+		DatabaseURL string `yaml:"database_url" env:"PG_DATABASE_URL"`
+		SourceURL   string `yaml:"source_url" env:"PG_DATABASE_URL"`
 	}
 
-	// RMQ -.
-	RMQ struct {
-		ServerExchange string `env-required:"true" yaml:"rpc_server_exchange" env:"RMQ_RPC_SERVER"`
-		ClientExchange string `env-required:"true" yaml:"rpc_client_exchange" env:"RMQ_RPC_CLIENT"`
-		URL            string `env-required:"true"                            env:"RMQ_URL"`
+	Trace struct {
+		URL   string `yaml:"url" env:"TRACER_URL" env-default:"http://localhost:14268/api/traces?service=customer-administration"`
+		Level int    `env-required:"true" yaml:"level"   env:"TRACE_LEVEL"`
+	}
+
+	HTTP struct {
+		Port string `yaml:"port" env:"HTTP_PORT"`
+	}
+
+	GRPC struct {
+		Port string `yaml:"port" env:"GRPC_PORT"`
+	}
+
+	KeyCloak struct {
+		URL          string `yaml:"url" env:"KEYCLOAK_URL" env-default:"https://tpm-keycloak.boquar.tech"`
+		UserName     string `yaml:"username" env:"KEYCLOAK_USERNAME" env-default:"acladminservice"`
+		ClientId     string `yaml:"clientid" env:"KEYCLOAK_CLIENTID" env-default:"aclaAdmin"`
+		ClientSecret string `yaml:"clientsecret" env:"KEYCLOAK_CLIENTSECRET" env-default:"Freedom123_!"`
+		Timeout      int    `yaml:"timeout" env:"KEYCLOAK_TIMEOUT"`
+		Realm        string `yaml:"realm" env:"KEYCLOAK_REALM"`
 	}
 )
 
-// NewConfig returns app config.
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	err := cleanenv.ReadConfig("config/config.yml", cfg)
 	if err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
+		return nil, err
 	}
 
 	err = cleanenv.ReadEnv(cfg)
